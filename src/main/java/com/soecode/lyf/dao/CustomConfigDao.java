7b16e96b4c2a04bbf7415512aa981f160d0e3a14
Fantasy <805841483@qq.com>
2017-01-08 22:46:20 +0800

    添加控制层的父级基本操作

--- src/main/java/com/soecode/lyf/dao/CustomConfigDao.java
+++ src/main/java/com/soecode/lyf/dao/CustomConfigDao.java
@@ -4,5 +4,5 @@ package com.soecode.lyf.dao;
  * Created by Fantasy on 2017/1/6.
  * 系统的后端的配置接口
  */
-public class CustomConfigDao {
+public interface CustomConfigDao {
 }
--- src/main/java/com/soecode/lyf/web/MenuMainController.java
+++ src/main/java/com/soecode/lyf/web/MenuMainController.java
@@ -6,6 +6,7 @@ import com.soecode.lyf.entity.Menu;
 import com.soecode.lyf.service.MenuMainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,15 +20,17 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("/Menus")
-public class MenuMainController {
+public class MenuMainController extends BECtrlDataController {
     @Autowired
     private MenuMainService menuMainService;
 
-    @ResponseBody
-    @RequestMapping(value = "/getMenuMain", method = RequestMethod.GET, produces = {"text/html;charset=UTF-8;", "application/json;"})
-    public String queryMenuMain() {
-        List<Menu> menusList = menuMainService.queryMainMenus();
-        List<Menu> newMenusList = new ArrayList<Menu>();
+    /**
+     * 封装menu数组
+     *
+     * @param menusList    数据库查询的全局数据
+     * @param newMenusList 转换的数据
+     */
+    public void packageMenusList(List<Menu> menusList, List<Menu> newMenusList) {
         for (Menu m : menusList) {
             List<Menu> subMenuList = new ArrayList<Menu>();
             for (Menu subMenu : menusList) {
@@ -40,9 +43,48 @@ public class MenuMainController {
                 newMenusList.add(m);
             }
         }
+    }
+
+    /**
+     * 为前端ajax 请求
+     *
+     * @return {String}
+     */
+    @ResponseBody
+    @RequestMapping(value = "/queryMenuMain", method = RequestMethod.GET, produces = {"text/html;charset=UTF-8;", "application/json;"})
+    public String queryMenuMain() {
+        List<Menu> newMenusList = new ArrayList<Menu>();
+        this.packageMenusList(menuMainService.queryMainMenus(), newMenusList);
         Result result = new Result<List>(true, newMenusList);
         result.setError("0");
         result.setReason("查询成功");
         return JSON.toJSON(result).toString();
+    }
+
+    @Override
+    public String addCtrl() {
+        return "";
+    }
+
+    @Override
+    public String delCtrl() {
+        return "";
+    }
+
+    @Override
+    public String updateCtrl() {
+        return "";
+    }
+
+    /**
+     * 为后端普通请求
+     * @param model 设置属性
+     * @return {String}
+     */
+    @Override
+    @RequestMapping(value = "/getMenuMain", method = RequestMethod.GET)
+    public String getCtrl(Model model) {
+        model.addAttribute("MenuMain", menuMainService.queryMainMenus());
+        return "";
     }
 }
