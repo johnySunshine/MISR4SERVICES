bb6f3d7a38fb636ca7bf1d6ca09c2e419c4a9a8a
Fantasy <805841483@qq.com>
2017-01-12 22:23:12 +0800

    配置项的编辑

---
+++ src/main/java/com/soecode/lyf/dao/RecommendHubDao.java
@@ -0,0 +1,7 @@
+package com.soecode.lyf.dao;
+
+/**
+ * Created by Fantasy on 2017/1/11.
+ */
+public interface RecommendHubDao {
+}
---
+++ src/main/java/com/soecode/lyf/entity/CustomConfig.java
@@ -0,0 +1,33 @@
+package com.soecode.lyf.entity;
+
+/**
+ * Created by Fantasy on 2017/1/11.
+ */
+public class CustomConfig {
+    private String key;
+    private String value;
+    private String userType;
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
