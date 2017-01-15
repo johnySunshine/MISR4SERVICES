f8bc28f278ad1e7f2c607ca60a08857897c0761e
Fantasy <805841483@qq.com>
2017-01-15 16:22:40 +0800

    增删该查的初始方法编写完成

--- src/main/java/com/soecode/lyf/web/CustomConfigController.java
+++ src/main/java/com/soecode/lyf/web/CustomConfigController.java
@@ -31,6 +31,7 @@ public class CustomConfigController implements BECtrlDataController<CustomConfig
      */
     public List<CustomConfig> getListFromTemp() {
         List<CustomConfig> configList;
+        this.cacheList = this.cacheList == null ? new ArrayList<CustomConfig>() : this.cacheList;
         if (this.cacheList.size() != 0) {
             configList = this.cacheList;
         } else {
@@ -40,9 +41,16 @@ public class CustomConfigController implements BECtrlDataController<CustomConfig
         return configList;
     }
 
-
+    /**
+     * 添加数据
+     *
+     * @param customConfig 配置对象
+     * @param model        视图
+     * @return {String}
+     */
+    @RequestMapping(value = "/addCusConfig", method = RequestMethod.POST)
     public String addCtrl(CustomConfig customConfig, Model model) {
-        this.cacheList.size();
+        this.cacheList = null;
         int operationStatus = configService.insertCustomConfig(customConfig);
         if (operationStatus == 0) {
             model.addAttribute("error_title", "配置添加");
@@ -54,12 +62,24 @@ public class CustomConfigController implements BECtrlDataController<CustomConfig
 
     public String delCtrl(String t, Model m) {
         this.cacheList = null;
-        return null;
+        int operationStatus = configService.deleteCustomConfig(Integer.parseInt(t));
+        if (operationStatus == 1) {
+            m.addAttribute("code_msg", "配置删除成功");
+        } else {
+            m.addAttribute("code_msg", "配置删除失败，可能已经删除");
+        }
+        return this.getConfigByUserType("webtv", m);
     }
 
     public String updateCtrl(CustomConfig customConfig, Model model) {
         this.cacheList = null;
-        return null;
+        int operationStatus = configService.updateCustomConfig(customConfig);
+        if (operationStatus == 1) {
+            model.addAttribute("code_msg", "配置修改成功");
+        } else {
+            model.addAttribute("code_msg", "配置修改成功，可能已经没有此菜单");
+        }
+        return this.getConfigByUserType("webtv", model);
     }
 
     public String getCtrl(Model model) {
@@ -69,8 +89,14 @@ public class CustomConfigController implements BECtrlDataController<CustomConfig
         return "template/showConfig";
     }
 
-
-    @RequestMapping(value = "/getConfigById", method = RequestMethod.GET)
+    /**
+     * 根据userType 查询数据
+     *
+     * @param userType
+     * @param m
+     * @return
+     */
+    @RequestMapping(value = "/getConfigByUserType", method = RequestMethod.GET)
     public String getConfigByUserType(String userType, Model m) {
         List<CustomConfig> configList = this.getListFromTemp();
         List<CustomConfig> tempConfig = new ArrayList<CustomConfig>();
