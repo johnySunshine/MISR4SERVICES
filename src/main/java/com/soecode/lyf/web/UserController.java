00a0f8018b91d8f004dccc645bd949e3f4ac9871
Fantasy <805841483@qq.com>
2017-01-18 23:14:04 +0800

    修复json jar错误问题，以及userController命名的更改

--- src/main/java/com/soecode/lyf/web/UserController.java
+++ src/main/java/com/soecode/lyf/web/UserController.java
@@ -8,9 +8,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
