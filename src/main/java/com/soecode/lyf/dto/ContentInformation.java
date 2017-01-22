acc97da7ac16cf58e60ca384e5bb1af627c52a9c
Fantasy <805841483@qq.com>
2017-01-22 22:30:45 +0800

    影片中的所有类的添加

---
+++ src/main/java/com/soecode/lyf/dto/ContentInformation.java
@@ -0,0 +1,9 @@
+package com.soecode.lyf.dto;
+
+/**
+ * Created by Fantasy on 2017/1/22.
+ * 影片的基本信息
+ */
+public class ContentInformation {
+
+}
---
+++ src/main/java/com/soecode/lyf/dto/MultiAssetInformation.java
@@ -0,0 +1,13 @@
+package com.soecode.lyf.dto;
+
+/**
+ * Created by Fantasy on 2017/1/22.
+ * seasonNumber
+     seasonTitle
+     series
+     seriesTitle
+     subAssetDetails
+ */
+public class MultiAssetInformation {
+
+}
