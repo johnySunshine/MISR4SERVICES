96e90131a103a1dea14bb3a1700ef8241d623f6e
Fantasy <805841483@qq.com>
2017-01-28 22:28:48 +0800

    添加电影操作

--- src/test/java/com/soecode/lyf/dao/MovieDaoTest.java
+++ src/test/java/com/soecode/lyf/dao/MovieDaoTest.java
@@ -15,8 +15,27 @@ public class MovieDaoTest extends BaseTest {
     private MovieDao movieDao;
 
     @Test
-    public void TestMovieDao(){
+    public void TestMovieDao() {
         List<Movie> movies = movieDao.queryDao();
         System.out.println(movies);
     }
+
+    @Test
+    public void TestAddMovieDao() {
+        Movie movie = new Movie();
+        movie.setTitle("影片标题");
+        movie.setCategory("影片类别");
+        movie.setChildProtectionDisplayName("影片保护等级显示");
+        movie.setChildProtectionId("影片儿童保护id");
+        movie.setCountries("影片国家");
+        movie.setCreationDate("影片创建时间");
+        movie.setLongDescription("影片描述");
+        movie.setMainGenre("影片主要类别");
+        movie.setRuntime("影片时长");
+        movie.setYear("影片年份");
+        movie.setOriginalTitle("影片原标题");
+        Integer i = movieDao.insertDao(movie);
+        System.out.println(i);
+    }
+
 }
