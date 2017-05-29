e07b99c2e9171a6945115de1e67951a16c1adefe
Fantasy <805841483@qq.com>
2017-05-29 19:15:12 +0800

    *更改数据源 *所有目录结构的更改

--- src/main/java/com/msir/web/MenuController.java
+++ src/main/java/com/msir/web/MenuController.java
@@ -1,11 +1,15 @@
 package com.msir.web;
 
+import com.alibaba.fastjson.JSON;
+import com.msir.enums.MenuStateEnum;
+import com.msir.pojo.Menu;
 import com.msir.pojo.MenuDO;
 import com.msir.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,7 +22,97 @@ public class MenuController {
     @Autowired
     private MenuService menuService;
 
-    public List<MenuDO> listMenu() {
-        return menuService.listMenu();
+    /**
+     * 得到所有menu数目
+     *
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/list", method = RequestMethod.GET, produces = {"application/json; charset=utf-8"})
+    public Object listMenu() {
+        FinalResult finalResult = new FinalResult<List>(
+                true,
+                menuService.listMenu(),
+                "查询成功",
+                "菜单列表",
+                MenuStateEnum.MENU_QUERY_SUCCESS.getStateValue());
+        return JSON.toJSON(finalResult);
     }
+
+    /**
+     * 添加单个menu数量
+     *
+     * @param menuDO
+     */
+    @ResponseBody
+    @RequestMapping(value = "/detail", method = RequestMethod.POST, produces = {"application/json; charset=utf-8"})
+    public Object saveMenu(MenuDO menuDO) {
+        FinalResult finalResult;
+        int menuServiceStatus = menuService.saveMenu(menuDO);
+        if (menuServiceStatus == 1) {
+            finalResult = resultChange(true, "新增菜单", "新增菜单成功", MenuStateEnum.MENU_SAVE_SUCCESS);
+        } else {
+            finalResult = resultChange(true, "新增菜单", "新增菜单失败", MenuStateEnum.MENU_SAVE_FAIL);
+        }
+        return JSON.toJSON(finalResult);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/detail", method = RequestMethod.PUT, produces = {"application/json; charset=utf-8"})
+    public Object updateMenu(@RequestBody MenuDO menuDO) {
+        FinalResult finalResultUpdate;
+        int menuServiceStatus = menuService.updateMenu(menuDO);
+        if (menuServiceStatus == 1) {
+            finalResultUpdate = resultChange(true, "更新菜单", "更新菜单成功", MenuStateEnum.MENU_UPDATE_SUCCESS);
+        } else {
+            finalResultUpdate = resultChange(true, "更新菜单", "更新菜单失败", MenuStateEnum.MENU_UPDATE_FAIL);
+        }
+        return JSON.toJSON(finalResultUpdate);
+    }
+
+    /**
+     * 得到单个菜单的详情
+     */
+    @ResponseBody
+    @RequestMapping(value = "/detail/{menuId}", method = RequestMethod.GET, produces = {"application/json; charset=utf-8"})
+    public Object getMenu(@PathVariable("menuId") int menuId) {
+        FinalResult finalResult = new FinalResult<MenuDO>(
+                true,
+                menuService.getMenu(menuId),
+                "查询成功",
+                "菜单详情",
+                MenuStateEnum.MENU_QUERY_SUCCESS.getStateValue());
+        return JSON.toJSON(finalResult);
+    }
+
+    /**
+     * 删除单个菜单
+     */
+    @ResponseBody
+    @RequestMapping(value = "/detail/{menuId}", method = RequestMethod.DELETE, produces = {"application/json; charset=utf-8"})
+    public Object removeMenu(@PathVariable("menuId") int menuId) {
+        FinalResult finalResultUpdate;
+        int menuServiceStatus = menuService.removeMenu(menuId);
+        if (menuServiceStatus == 1) {
+            finalResultUpdate = resultChange(true, "删除菜单", "删除菜单成功", MenuStateEnum.MENU_REMOVE_SUCCESS);
+        } else {
+            finalResultUpdate = resultChange(true, "删除菜单", "删除菜单失败", MenuStateEnum.MENU_REMOVE_FAIL);
+        }
+        return JSON.toJSON(finalResultUpdate);
+    }
+
+    private void packageMenusList(List<MenuDO> menusList, List<MenuDO> newMenusList) {
+
+    }
+
+    private FinalResult resultChange(boolean status, String title, String messages, MenuStateEnum menuStateEnum) {
+        FinalResult finalResult = new FinalResult<String>();
+        finalResult.setRetCode(menuStateEnum.getStateValue());
+        finalResult.setStatus(status);
+        finalResult.setMessages(messages);
+        finalResult.setTitle(title);
+        finalResult.setResult("");
+        return finalResult;
+    }
+
 }
