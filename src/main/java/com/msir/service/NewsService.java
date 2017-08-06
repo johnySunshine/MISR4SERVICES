252a783f1b7b193f88300c71efed08c418ce6cfc
Fantasy <805841483@qq.com>
2017-08-07 00:00:34 +0800

    2017-8-4 News资讯的基本构建

--- src/main/java/com/msir/service/NewsService.java
+++ src/main/java/com/msir/service/NewsService.java
@@ -1,11 +1,13 @@
 package com.msir.service;
 
+
 /**
  * Created by Fantasy on 2017/8/3.
  * 关于新闻的接口
  */
+
 public interface NewsService {
     Object getMoveList4One();
-
-    Object getMoveDetail4One();
+    Object getMoveDetail4One(String id);
+    Object getMoveListHistory4One(String historyId);
 }
--- src/main/java/com/msir/utils/HttpUtils.java
+++ src/main/java/com/msir/utils/HttpUtils.java
@@ -1,33 +1,50 @@
 package com.msir.utils;
 
+import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Map;
 
 public abstract class HttpUtils {
+    private static String schemeName;
+    private static String hostName;
+    private static String pathName;
+    private static int port;
 
-    public static URI converseURI(String schemeName, String hostName, String pathName, List<Map<String, String>> params) {
+    public static void setSchemeName(String schemeName) {
+        HttpUtils.schemeName = schemeName;
+    }
+
+    public static void setHostName(String hostName) {
+        HttpUtils.hostName = hostName;
+    }
+
+    public static void setPathName(String pathName) {
+        HttpUtils.pathName = pathName;
+    }
+
+    public static void setPort(int port) {
+        HttpUtils.port = port;
+    }
+
+    public static URI converseURI(List<NameValuePair> params) {
         URI apiURL = null;
         URIBuilder urlBuilder = new URIBuilder();
         try {
-            urlBuilder.setHost(hostName);
-            urlBuilder.setPath(pathName);
-            urlBuilder.setParameters();
-            if (null != params || params.isEmpty()) {
-                for (Map<String, String> map : params) {
-                    if (null != map) {
-                        urlBuilder.setParameter(map.get(""),map.get(""));
-                    }
-                }
+            urlBuilder.setScheme(HttpUtils.schemeName);
+            urlBuilder.setHost(HttpUtils.hostName);
+            urlBuilder.setPath(HttpUtils.pathName);
+            if (HttpUtils.port != 0) {
+                urlBuilder.setPort(HttpUtils.port);
             }
+            urlBuilder.setParameters(params);
             apiURL = urlBuilder.build();
-
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
         return apiURL;
     }
+
 }
