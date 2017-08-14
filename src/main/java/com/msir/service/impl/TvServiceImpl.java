ccd99361a03a666e77658df041c5b23ca7380843
Fantasy <805841483@qq.com>
2017-08-14 21:24:28 +0800

    2017-8-14 tv结构调整

---
+++ src/main/java/com/msir/service/impl/TvServiceImpl.java
@@ -0,0 +1,66 @@
+package com.msir.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.msir.service.TvService;
+import com.msir.utils.GlobalUtils;
+import com.msir.utils.HttpUtils;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Fantasy on 2017/8/14.
+ * 电视节目信息
+ */
+@Service
+public class TvServiceImpl implements TvService {
+    final String API_KEY = "e797c5d6ccd36ae12f073ca69297c185";
+    final String HOST_NAME = "japi.juhe.cn";
+
+    public Object getCategory() {
+        List<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+        this.initHttps(postParameters);
+        HttpUtils.setPathName("/tv/getCategory");
+        URI uri = HttpUtils.converseURI(postParameters);
+        return JSON.toJSON(GlobalUtils.httpsManager4get(uri));
+    }
+
+    public Object getChannel(String pId) {
+        List<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+        this.initHttps(postParameters);
+        HttpUtils.setPathName("/tv/getChannel");
+        postParameters.add(new BasicNameValuePair("pId", pId));
+        URI uri = HttpUtils.converseURI(postParameters);
+        return JSON.toJSON(GlobalUtils.httpsManager4get(uri));
+    }
+
+    public Object getProgram(String channelCode, String date) {
+        List<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+        this.initHttps(postParameters);
+        HttpUtils.setPathName("/tv/getProgram");
+        postParameters.add(new BasicNameValuePair("channelCode", channelCode));
+        postParameters.add(new BasicNameValuePair("date", date));
+        URI uri = HttpUtils.converseURI(postParameters);
+        return JSON.toJSON(GlobalUtils.httpsManager4get(uri));
+    }
+
+    public Object getHubHot() {
+        HttpUtils.setSchemeName("http");
+        HttpUtils.setHostName("eye.kuyun.com");
+        HttpUtils.setPathName("/api/tvlb");
+        HttpUtils.setPort(0);
+        URI uri = HttpUtils.converseURI();
+        return JSON.toJSON(GlobalUtils.httpsManager4get(uri));
+    }
+
+    private void initHttps(List<NameValuePair> postParameters) {
+        HttpUtils.setSchemeName("http");
+        HttpUtils.setHostName(HOST_NAME);
+        HttpUtils.setPort(0);
+        postParameters.add(new BasicNameValuePair("key", API_KEY));
+    }
+}
--- src/main/java/com/msir/web/TVController.java
+++ src/main/java/com/msir/web/TVController.java
@@ -1,95 +1,46 @@
 package com.msir.web;
 
-import com.alibaba.fastjson.JSON;
-import com.msir.utils.GlobalUtils;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.ibatis.annotations.Param;
+import com.msir.service.TvService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-/**
- * Created by Fantasy on 2017/1/3.
- */
 @Controller
-@RequestMapping("/tv")
+@RequestMapping("/TV")
 public class TVController {
-    final String CHANNEL_APP_KEY = "e797c5d6ccd36ae12f073ca69297c185";
-    final String TV_HOST = "japi.juhe.cn";
+
+    @Autowired
+    private TvService tvService;
+
+    //1.热播电视剧
+    @ResponseBody
+    @RequestMapping(value = "/hotChannel", method = RequestMethod.GET, produces = {"application/json; charset=utf-8"})
+    public Object getHubHot() {
+        return tvService.getHubHot();
+    }
 
     //1.电视台分类
     @ResponseBody
     @RequestMapping(value = "/category", method = RequestMethod.GET, produces = {"application/json; charset=utf-8"})
     public Object getCategory() {
-        URI apiURL = null;
-        try {
-            apiURL = new URIBuilder()
-                    .setScheme("https")
-                    .setHost(TV_HOST)
-                    .setPath("/tv/getCategory")
-                    .setParameter("key", CHANNEL_APP_KEY)
-                    .build();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        return JSON.toJSON(GlobalUtils.httpsManager4get(apiURL));
+        return tvService.getCategory();
     }
 
     //2.电视频道列表
     @ResponseBody
     @RequestMapping(value = "/channel/{pId}", method = RequestMethod.GET, produces = {"application/json; charset=utf-8"})
     public Object getChannel(@PathVariable("pId") String pId) {
-        URI apiURL = null;
-        try {
-            apiURL = new URIBuilder()
-                    .setScheme("https")
-                    .setHost(TV_HOST)
-                    .setPath("/tv/getChannel")
-                    .setParameter("key", CHANNEL_APP_KEY)
-                    .setParameter("pId", pId)
-                    .build();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        return JSON.toJSON(GlobalUtils.httpsManager4get(apiURL));
+        return tvService.getChannel(pId);
     }
 
     //3.电视台节目单列表
     @ResponseBody
-    @RequestMapping(value = "/programList", method = RequestMethod.GET, produces = {"application/json; charset=utf-8"})
-    public Object getProgram(@Param("pCode") String pCode, @Param("date") String date) {
-        URI apiURL = null;
-        System.out.println(pCode);
-        try {
-            if (null != date) {
-                apiURL = new URIBuilder()
-                        .setScheme("https")
-                        .setHost(TV_HOST)
-                        .setPath("/tv/getProgram")
-                        .setParameter("key", CHANNEL_APP_KEY)
-                        .setParameter("code", pCode)
-                        .setParameter("date", "")
-                        .build();
-
-            } else {
-                apiURL = new URIBuilder()
-                        .setScheme("https")
-                        .setHost(TV_HOST)
-                        .setPath("/tv/getProgram")
-                        .setParameter("key", CHANNEL_APP_KEY)
-                        .setParameter("code", pCode)
-                        .setParameter("date", date)
-                        .build();
-            }
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        return JSON.toJSON(GlobalUtils.httpsManager4get(apiURL));
+    @RequestMapping(value = "/programList/{pCode}/{date}", method = RequestMethod.GET, produces = {"application/json; charset=utf-8"})
+    public Object getProgram(@PathVariable("pCode") String pCode, @PathVariable("date") String date) {
+        return tvService.getProgram(pCode, date);
     }
 
 }
